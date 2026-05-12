6ded2a1f4ae453c50052600fee51ad27d24354ca
The-Law-1 <gabriel.griffin@epitech.eu>
2026-05-12 22:53:29 +0100

    🚚 Using a web hash history

--- src/router.ts
+++ src/router.ts
@@ -1,21 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 
 import Engine from './pages/Engine.vue'
 import Games from './pages/Games.vue'
 import Pgn from './pages/Pgn.vue'
 
-const root = '/chess-insights'
-
 const routes = [
-  { path: root, component: Games },
-  { path: `${root}/engine`, component: Engine },
-  { path: `${root}/pgn`, component: Pgn },
-  { path: `${root}/games`, component: Games },
-  { path: '/:pathMatch(.*)*', redirect: root },
+  { path: "/", component: Games },
+  { path: "/engine", component: Engine },
+  { path: "/pgn", component: Pgn },
+  { path: "/games", component: Games },
+  { path: "/:pathMatch(.*)*", redirect: "/" },
 ]
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
