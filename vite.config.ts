460d4302a543c5608b4268c797352c93d502757e
The-Law-1 <gabriel.griffin@epitech.eu>
2026-05-14 21:35:58 +0100

    🐛 Debugging on mobile

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 const crossOriginHeaders = {
-  'Cross-Origin-Embedder-Policy': 'require-corp',
+  'Cross-Origin-Embedder-Policy': 'credentialless',
   'Cross-Origin-Opener-Policy': 'same-origin',
 }
 
